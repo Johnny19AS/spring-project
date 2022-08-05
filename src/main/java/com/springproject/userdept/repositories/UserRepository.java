3b29aab99866213ab60f09f58d27da069d8beeff
origin <https://github.com/Johnny19AS/desafio-poo-dio.git>
2022-08-05 16:44:18 -0300

    Initial commit

---
+++ src/main/java/com/springproject/userdept/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.springproject.userdept.repositories;
+
+import com.springproject.userdept.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
